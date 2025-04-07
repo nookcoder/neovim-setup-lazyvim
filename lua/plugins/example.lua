29be693dee0cd6f3c479bae1336457675f640e10
Hyeonuk Kim <hyeonuk.kim@youdongsan.com>
2025-04-07 14:25:06 +0900

    add: add colorscheme named gruvbox

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -10,7 +10,16 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night", transparent = true, },
+  },
+  { "ellisonleao/gruvbox.nvim", lazy = true,
+    opts = {
+    transparent_mode = true,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
@@ -202,7 +211,7 @@ return {
     },
 
     -- use mini.starter instead of alpha
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
     -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
     { import = "lazyvim.plugins.extras.lang.json" },
@@ -227,10 +236,10 @@ return {
       },
     },
 
-    {
-      "leoluz/nvim-dap-go",
-      opts = {},
-    },
+    -- {
+    --   "leoluz/nvim-dap-go",
+    --   opts = {},
+    -- },
 
     {
       "nvimtools/none-ls.nvim",
